9309eee2029001bacdc03d855a282b6f06db4168
Milos Zivlak <zivlakmilos@gmail.com>
2023-07-09 17:55:50 +0200

    Top margin

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "compiling pdf..."
-pandoc -f gfm -t html5 --metadata pagetitle="cv.md" --css style.css cv.md -o cv.pdf
+pandoc -f gfm -t html5 --metadata pagetitle="cv.md" --css style.css -V margin-top=10 -V margin-left=3 -V margin-right=3 -V margin-bottom=3 -V papersize=a4 cv.md -o cv.pdf
 
 echo "compiling html page..."
 pandoc --standalone --template template.html cv.md -o index.html
